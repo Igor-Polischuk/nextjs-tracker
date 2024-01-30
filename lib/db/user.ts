9f07756cda2b81ed66d69e525d66a0d69e43b86c
Igor Polischuk <igor.polischuk@sysgears.com>
2024-01-30 13:08:04 +0200

    handle no user case

--- lib/db/user.ts
+++ lib/db/user.ts
@@ -1,15 +1,33 @@
 import { prisma } from ".";
 
 export async function getCurrentUser() {
-    const user = await prisma.user.findFirst({
-        where: {
-            username: 'Igor',
-        }
-    })
+  const user = await prisma.user.findFirst({
+    where: {
+      username: "Igor",
+    },
+  });
 
-    if (!user) {
-        throw new Error('User not found')
-    }
+  if (!user) {
+    // TODO removed it when implement auth logic
+    return createUser({});
+    // throw new Error('User not found')
+  }
 
-    return user;
+  return user;
+}
+// TODO refactor it when implement auth logic
+async function createUser(data: unknown) {
+  const user = prisma.user.create({
+    data: {
+      name: "Igor",
+      age: 21,
+      height: 180,
+      sex: "MALE",
+      username: "Igor",
+      weight: 78,
+      activityLevel: "LIGHT_EXERCISE",
+    },
+  });
+
+  return user;
 }
